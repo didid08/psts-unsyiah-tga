7b026ee483181605a793b533294372cc5596c4bd
Didid <diva.mujaddidi@outlook.com>
2020-12-14 15:39:55 +0700

    Updated

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => 'smtp.gmail.com',
-            'port' => 587,
-            'encryption' => 'tls',
-            'username' => 'psts.unsyiah@gmail.com',
-            'password' => 'pstsunsyiah123',
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME', 'psts.unsyiah@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'pstsunsyiah123'),
             'timeout' => null,
             'auth_mode' => null,
         ],
